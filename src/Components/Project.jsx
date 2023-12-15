17694e98bcdabbdfd712de5757d03de2eea75ffd
unknown <fayhadahmedmonir@gmail.com>
2023-12-15 19:23:46 +0600

    added the project section

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -22,7 +22,7 @@ const Project = () => {
 <div className="  content-shadow rounded-md  md:mb-20 flex-1 ">
 <div className='p-4  text-[#878e99] md:text-lg font-medium space-y-3 '>
 <img className='rounded-md' src="https://i.ibb.co/VQL11PR/beautiful2.jpg" alt="" />
-<h1 className='md:text-3xl font-bold text-[#c4cfde]'>Tourism Guide</h1>
+<h1 className='md:text-3xl font-bold text-[#c4cfde]'>Event Management</h1>
 <div className="navbar-start mt-8 ">
 <a href="https://capable-cast.surge.sh/" className="btn  text-white   bg-[#FF014F] border-none hover:bg-[#FF014F] hover:-translate-y-1.5">VISIT SITE</a>
 </div>
